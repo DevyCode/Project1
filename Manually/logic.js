6a25863f37021e8ef6bc387d5a74a06e4fd20c1e
DevyCode <devycode@outlook.com>
2023-04-04 21:59:51 +0200

    Animation and quotes

--- Manually/logic.js
+++ Manually/logic.js
@@ -1,2 +1,33 @@
+const quotes = [
+    {quote: "quote 1", author: "author 1"},
+    {quote: "quote 2", author: "author 2"},
+    {quote: "quote 3", author: "author 3"},
+    {quote: "quote 4", author: "author 4"},
+    {quote: "quote 5", author: "author 5"},
+    {quote: "quote 6", author: "author 6"},
+    {quote: "quote 7", author: "author 7"}, 
+]
 
+const button = document.querySelector('.btn');
+const quoteText = document.querySelector('#blockquote');
+const author    = document.querySelector('#author');
 
+  button.addEventListener('click',(e)=>{
+    e.preventDefault();
+    button.classList.add('btn--clicked');
+    document.querySelectorAll('span').forEach((element)=>{element.classList.add('expanded')})
+    
+    
+    /* 
+---------------------------------------
+just to reset without having to refresh..
+ --------------------------------*/
+     setTimeout(()=>{button.classList.remove("btn--clicked")},3500);
+     setTimeout(()=>{document.querySelectorAll('span').forEach((element)=>{element.classList.remove('expanded')})},1700)
+     setTimeout(()=> {
+        const randomIndex = Math.floor(Math.random()*quotes.length)
+        quoteText.innerText = quotes[randomIndex]['quote']
+        author.innerText    = quotes[randomIndex]['author']},1750);
+     
+    }
+  )
